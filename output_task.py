76c3bea4658e50a98d191a49f84fa5e8b7fe115f
BePower1001 <adrian.pytlak@outlook.com>
2025-11-23 08:31:16 +0000

    Full task completed

--- output_task.py
+++ output_task.py
@@ -1,1 +1,5 @@
-print ("Welcome to QH4444")
+print("Welcome to QH4444")
+print("In week 1 we will learn...")
+print("...How to use Git and GitHub")
+print("...How to output to the screen")
+print("...How to get user input")
