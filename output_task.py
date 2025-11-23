e9ba6a1f7190ddca3cb8347a874160226971012b
BePower1001 <adrian.pytlak@outlook.com>
2025-11-23 08:36:08 +0000

    Im programming

--- output_task.py
+++ output_task.py
@@ -3,3 +3,10 @@ print("In week 1 we will learn...")
 print("...How to use Git and GitHub")
 print("...How to output to the screen")
 print("...How to get user input")
+
+
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a backslash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
